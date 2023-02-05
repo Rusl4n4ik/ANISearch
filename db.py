f3f661c5f91947d5160d53f30801926ede726ff1
Rusl4n4ik <russharipov5328059@gmail.com>
2023-02-05 10:16:47 +0500

    Almost done

---
+++ db.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+    Column, DateTime, ForeignKey, Numeric
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+DATABASE_NAME = 'bot.sqlite'
+
+engine = create_engine(f"sqlite:///{DATABASE_NAME}")
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+def create_db():
+    Base.metadata.create_all(engine)
+    session = Session()
+    session.commit()
+
+
+class Users(Base):
+    __tablename__ = 'Clients'
+    id = Column(Integer, primary_key=True)
+    chat_id = Column(Integer)
+    first_name = Column(String(100), nullable=True)
+    username = Column(String(50), nullable=True)
+
+
+def add_user(id, first_name, username):
+    session = Session()
+    exist = check_existing(id)
+    if not exist:
+        user = Users(chat_id=id,
+                       first_name=first_name,
+                       username=username)
+        session.add(user)
+    session.commit()
+    session.close()
+
+
+def check_existing(id):
+    session = Session()
+    result = session.query(Users.chat_id).filter(Users.chat_id == id).all()
+    return result
+
+
+if __name__ == '__main__':
+    create_db()
